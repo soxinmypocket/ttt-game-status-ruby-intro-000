66f1c1d94951e121d5c9a50cca51fef93bf10891
mathematical-integer-7084 <shawnaejohnson6@gmail.com>
2020-03-16 05:08:22 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -10,7 +10,17 @@ WIN_COMBINATIONS = [
                     [6, 7, 8],
                     [0, 3, 6],
                     [1, 4, 7],
-                    [2, 5, 8], 
+                    [2, 5, 8],
                     [0, 4, 8],
                     [2, 4, 6]
 ]
+
+def won?(board)
+  WIN_COMBINATIONS.each {|win_combo|}
+  index_0 = win_combo |0|
+  index_1 = win_combo |1|
+  index_2 = win_combo |2|
+
+  position_1 = board[index_0]
+  position_2 = board[index_1]
+  position_3 = board[index_2]
